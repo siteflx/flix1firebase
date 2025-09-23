6777b58d5be39f0204b0c510bbb5f4d107b8658c
Gineide Anjos <gineideanjos5@gmail.com>
2025-09-23 14:42:17 +0000

    em comedias coloque o nomes destaque e abaixo das capas das fotos retire

--- src/components/video-thumbnail.tsx
+++ src/components/video-thumbnail.tsx
@@ -30,10 +30,6 @@ export function VideoThumbnail({ video, aspectRatio, rank }: VideoThumbnailProps
         <div className="absolute inset-0 flex items-center justify-center">
             <PlayCircle className="w-12 h-12 text-white/70 opacity-0 group-hover:opacity-100 transition-opacity transform-gpu group-hover:scale-110" />
         </div>
-        <div className="absolute bottom-0 left-0 right-0 p-2 bg-gradient-to-t from-black/80 to-transparent">
-              <p className="text-white text-sm font-semibold truncate">{video.title}</p>
-        </div>
-
       </div>
     </Link>
   );
